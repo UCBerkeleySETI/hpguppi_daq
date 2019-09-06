f2329c5e0ac6e2ac2e44bc62c90062f09a03b35e
David MacMahon <davidm@astro.berkeley.edu>
2019-09-06 09:14:50 -0700

    Add script for starting MeerKAT pipeline
    This includes lots of parameters that are specific to some 4K mode, 64
    antenna, 1 stream pcap test files that we have.

---
+++ src/meerkat_init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# hpguppi_meerkat.sh - A wrapper around hpguppi_init.sh for running
+# hpguppi_daq on MeerKAT.
+
+# These are based on test pcap files from a 4K mode single stream pcap file (OBSFREQ is faked)
+fenchan=4096
+nants=64
+nstrm=1
+hntime=256
+hnchan=16
+schan=2688
+chan_bw="0.208984375"
+obsbw="3.34375"
+obsfreq="1420"
+obsnchan=$((hnchan * nstrm * nants))
+hclocks=2097152
+
+$(dirname $0)/hpguppi_init.sh meerkat 0 \
+  -o DESTIP="0.0.0.0" \
+	-o FENCHAN=${fenchan} \
+	-o NANTS=${nants} \
+	-o NSTRM=${nstrm} \
+	-o HNTIME=${hntime} \
+	-o HNCHAN=${hnchan} \
+	-o SCHAN=${schan} \
+	-o CHAN_BW=${chan_bw} \
+  -o OBSBW=${obsbw} \
+  -o OBSFREQ=${obsfreq} \
+	-o OBSNCHAN=${obsnchan} \
+  -o HCLOCKS=${hclocks} \
+  "${@}"
+
+sleep 1
+
+hashpipe_check_status -k NDROP -i 0
+
