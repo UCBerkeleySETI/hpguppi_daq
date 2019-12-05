0a9b20923f0dc553a8b85706372a322929050538
David MacMahon <davidm@astro.berkeley.edu>
2019-12-05 19:11:42 +0200

    Ensure that PIPERBLK is a power of two
    This prevents the number of time samples per block from having large
    prime factors which can impact efficiency of subsequent block-based
    FFTs.

--- src/hpguppi_mkfeng.h
+++ src/hpguppi_mkfeng.h
@@ -196,6 +196,15 @@ struct mk_obs_info {
 
 #define MK_OBS_INFO_INVALID_SCHAN (-1)
 
+// Returns the largest power of two that it less than or equal to x.
+// Returns 0 if x is 0.
+static inline
+uint32_t
+prevpow2(uint32_t x)
+{
+  return x == 0 ? 0 : (0x80000000 >> __builtin_clz(x));
+}
+
 // Initialize all mk_obs_info fields to invalid values
 static inline
 void
@@ -293,12 +302,21 @@ mk_block_chan(const struct mk_obs_info oi, const struct mk_feng_spead_info fesi)
 // not be processed (e.g. copied to output files) so it is important to update
 // the BLOCSIZE field in the status buffer accordingly whenever a new
 // pktidx_per_block value is calculated.
+//
+// Furthermore, the number of time samples per block is desired to be a power
+// of two, so that subsequent FFTs can operate on complete blocks with maximum
+// efficiency.  For this to happen, both the number of time samples per heap
+// (HNTIME) and the number of PKTIDX values per block (PIPERBLK) must be powers
+// of two.  HNTIME is set by the upstream F engines, so we have no control of
+// that, but we can and do ensure that PIPERBLK is a power of 2.
+//
 // This function assumes each sample is 4 bytes ([1 real + 1 imag] * 2 pols).
 static inline
 uint32_t
 calc_mk_pktidx_per_block(size_t block_size, uint32_t nchan, uint32_t hntime)
 {
-  return (uint32_t)(block_size / (4 * nchan * hntime));
+  uint32_t piperblk = (uint32_t)(block_size / (4 * nchan * hntime));
+  return prevpow2(piperblk);
 }
 
 static inline
