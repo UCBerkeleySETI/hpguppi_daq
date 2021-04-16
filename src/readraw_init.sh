1731ce2b669a9ca0aac57dfe58789f413978c42d
Cherry Ng <cherrywyng@gmail.com>
2021-04-16 17:03:46 +0200

    init for readraw

--- src/readraw_init.sh
+++ src/readraw_init.sh
@@ -8,8 +8,6 @@
 #     output - (optional)
 #
 
-#basefile=""         #Input dir
-#outdir=${basefile}  #Output dir, replace if wants to write files to a different location
 hpguppi_plugin=/home/cherryng/softwares/hpguppi_daq/Dev-branch/hpguppi_daq/src/.libs/hpguppi_daq.so
 
 #Supported modes
@@ -17,18 +15,39 @@ if [ "$1" = 'readonly' ]
 then
     net_thread=hpguppi_read_raw_files
     out_thread=null_output_thread
-    basefile=$2
-    outdir=${basefile}
-    shift
+    if [ -z "$2" ]
+    then
+	echo Input dir not provided.
+	echo Usage: readraw_init.sh readonly [input]
+	echo Exiting...
+       exit
+    else
+	basefile=$2
+	outdir=${basefile}
+	shift
+    fi
 elif [ "$1" = 'cp' ]
 then
     net_thread=hpguppi_read_raw_files
     out_thread=hpguppi_rawdisk_only_thread
-    basefile=$2
-    outdir=$3
-    shift
+    if [ -z "$2" ] || [ -z "$3" ]
+    then
+	echo Input/Output dir not provided.
+	echo Usage: readraw_init.sh cp [input] [output]
+	echo Exiting...
+	exit
+    else
+	basefile=$2
+	outdir=$3
+	shift
+    fi
+else
+    echo Supported mode. Choose between \"readonly\" and \"cp\".
+    echo Exiting...
+    exit
 fi
 
+#-------------------------------------------
 echo "Run Command:" hashpipe -p ${hpguppi_plugin:-hpguppi_daq} $net_thread $out_thread \
  -o BASEFILE=${basefile} \
  -o OUTDIR=${outdir}
